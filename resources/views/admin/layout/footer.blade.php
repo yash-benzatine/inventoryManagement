6df3ca93c71e0f8f671723022ad3ce8d9d7843d2
Bhumika Khakhadiya <bhumika@benzatine.com>
2024-03-28 17:32:13 +0530

    some changes

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -12,28 +12,28 @@
 
                     </script>,
                     made with <i class="fa fa-heart"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
+                    <a href="https://www.benzatine.com" class="font-weight-bold" target="_blank">Benzatine</a>
                     &
-                    <a href="https://www.updivision.com" class="font-weight-bold" target="_blank">UPDIVISION</a>
+                    <a href="https://www.benzatine.com" class="font-weight-bold" target="_blank">UPDIVISION</a>
                     for a better web.
                 </div>
             </div>
             <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
-                        <a href="https://www.updivision.com" class="nav-link text-muted" target="_blank">UPDIVISION</a>
+                        <a href="https://www.benzatine.com" class="nav-link text-muted" target="_blank">UPDIVISION</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
+                        <a href="https://www.benzatine.com" class="nav-link text-muted" target="_blank">Benzatine</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
+                        <a href="https://www.benzatine.com/about-us" class="nav-link text-muted" target="_blank">About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
+                        <a href="https://www.benzatine.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                        <a href="https://www.benzatine.com/privacy-policy" class="nav-link pe-0 text-muted" target="_blank">License</a>
                     </li>
                 </ul>
             </div>
