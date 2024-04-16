7bed0ac164914774779b4eecdae062a605247ea0
Bhumika Khakhadiya <bhumika@benzatine.com>
2024-04-16 12:02:40 +0530

    changes

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -2,8 +2,8 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="{{ route('home') }}">
-            <img src="{{ asset('img/logo-ct-dark.png') }}" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-1 font-weight-bold">Inventory Management</span>
+            <img src="{{ asset('admin/logo-dark.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+            <span class="ms-1 font-weight-bold text-xs">Inventory Management</span>
         </a>
     </div>
     <hr class="horizontal dark mt-0">
